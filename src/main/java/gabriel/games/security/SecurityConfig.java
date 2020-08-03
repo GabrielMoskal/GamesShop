3f69fba12da3e54c60000ac8bc02b09515cfbe38
Gabriel <gab.mos92@gmail.com>
2020-08-03 14:51:21 +0200

    Added simple navbar view

--- src/main/java/gabriel/games/security/SecurityConfig.java
+++ src/main/java/gabriel/games/security/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .formLogin()
                     .loginPage("/login")
-                    .defaultSuccessUrl("/home");
+                    .defaultSuccessUrl("/");
 
     }
 
