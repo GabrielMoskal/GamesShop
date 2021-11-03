012ca5ddd43c433a08b39d8b5039639832e5706a
Gabriel <gab.mos92@gmail.com>
2021-11-03 23:31:14 +0100

    Added schemas and data for games, extracted user schemas and data

---
+++ src/main/resources/data_game.sql
@@ -0,0 +1,76 @@
+DELETE FROM game;
+
+INSERT INTO game(game_name) VALUES ('Age of Empires IV');
+INSERT INTO game(game_name) VALUES ('Europa Universalis IV');
+INSERT INTO game(game_name) VALUES ('World of Warcraft');
+
+DELETE FROM game_description;
+
+INSERT INTO game_description(id, description_short, webpage, rating_players, rating_reviewer)
+    VALUES (1,
+        'Age of Empires IV jest czwartą odsłoną bestsellerowego cyklu RTS-ów, ' ||
+        'nad którym pieczę wydawniczą sprawuje firma Microsoft. W przeciwieństwie do ' ||
+        'poprzednich części serii, tworzonych przez zespół Ensemble Studios, produkcja ' ||
+        'została opracowana przez studio Relic Entertainment, czyli autorów dwóch ' ||
+        'pierwszych gier spod szyldu Homeworld, serii Warhammer 40,000: Dawn of War' ||
+        ' oraz cyklu Company of Heroes.',
+        'https://www.ageofempires.com/games/age-of-empires-iv/', 10.0, 9.5);
+
+INSERT INTO game_description(id, description_short, webpage, rating_players, rating_reviewer)
+    VALUES (2,
+        'Czwarta odsłona cieszącego się dużą popularnością cyklu gier strategicznych ' ||
+        'Europa Universalis. Tytuł wyprodukowany został przez' ||
+        ' ekipę deweloperską Paradox Development Studio.',
+        'https://www.paradoxplaza.com/europa-universalis-all/', 9.0, 8.5);
+
+INSERT INTO game_description(id, description_short, webpage, rating_players, rating_reviewer)
+    VALUES (3,
+        'Epicka gra z gatunku MMORPG, której akcję osadzono w uniwersum znanym ' ||
+        'z kultowej serii RTS-ów fantasy firmy Blizzard.',
+        'https://worldofwarcraft.com/en-us/', 8.0, 7.5);
+
+DELETE FROM platform;
+
+INSERT INTO platform(id, platform_name) VALUES (1, 'PC');
+INSERT INTO platform(id, platform_name) VALUES (2, 'XBOX 360');
+INSERT INTO platform(id, platform_name) VALUES (3, 'Playstation 5');
+
+DELETE FROM game_platform;
+
+INSERT INTO game_platform(game_id, platform_id) VALUES (1, 1);
+INSERT INTO game_platform(game_id, platform_id) VALUES (1, 2);
+INSERT INTO game_platform(game_id, platform_id) VALUES (2, 1);
+INSERT INTO game_platform(game_id, platform_id) VALUES (3, 3);
+
+DELETE FROM game_release_date;
+
+INSERT INTO game_release_date(game_id, game_platform_id, release_date) VALUES (1, 1, '2021-10-28');
+INSERT INTO game_release_date(game_id, game_platform_id, release_date) VALUES (1, 2, '2021-10-30');
+INSERT INTO game_release_date(game_id, game_platform_id, release_date) VALUES (2, 1, '2013-08-13');
+INSERT INTO game_release_date(game_id, game_platform_id, release_date) VALUES (3, 3, '2005-02-11');
+
+DELETE FROM producer;
+
+INSERT INTO producer(id, producer_name) VALUES (1, 'Relic Entertainment');
+INSERT INTO producer(id, producer_name) VALUES (2, 'World''s Edge');
+INSERT INTO producer(id, producer_name) VALUES (3, 'Paradox Development Studio');
+INSERT INTO producer(id, producer_name) VALUES (4, 'Blizzard Entertainment');
+
+DELETE FROM game_producer;
+
+INSERT INTO game_producer(game_id, producer_id) VALUES (1, 1);
+INSERT INTO game_producer(game_id, producer_id) VALUES (1, 2);
+INSERT INTO game_producer(game_id, producer_id) VALUES (2, 3);
+INSERT INTO game_producer(game_id, producer_id) VALUES (3, 4);
+
+DELETE FROM publisher;
+
+INSERT INTO publisher(id, publisher_name) VALUES (1, 'Xbox Game Studios / Microsoft Studios');
+INSERT INTO publisher(id, publisher_name) VALUES (2, 'Paradox Interactive');
+INSERT INTO publisher(id, publisher_name) VALUES (3, 'Blizzard Entertainment');
+
+DELETE FROM game_publisher;
+
+INSERT INTO game_publisher(game_id, publisher_id) VALUES (1, 1);
+INSERT INTO game_publisher(game_id, publisher_id) VALUES (2, 2);
+INSERT INTO game_publisher(game_id, publisher_id) VALUES (3, 3);
---
+++ src/main/resources/schema_game.sql
@@ -0,0 +1,60 @@
+CREATE TABLE IF NOT EXISTS game (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS game_description (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    description_short TEXT,
+    webpage VARCHAR(256),
+    rating_players DECIMAL(4, 2),
+    rating_reviewer DECIMAL(4, 2)
+);
+
+CREATE TABLE IF NOT EXISTS platform (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    platform_name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS game_platform (
+    game_id INT NOT NULL,
+    platform_id INT NOT NULL
+);
+
+ALTER TABLE game_platform ADD FOREIGN KEY (game_id) REFERENCES game(id);
+ALTER TABLE game_platform ADD FOREIGN KEY (platform_id) REFERENCES platform(id);
+
+CREATE TABLE IF NOT EXISTS game_release_date (
+    game_id INT NOT NULL,
+    game_platform_id INT NOT NULL,
+    release_date DATE
+);
+
+ALTER TABLE game_release_date ADD FOREIGN KEY (game_id) REFERENCES game(id);
+ALTER TABLE game_release_date ADD FOREIGN KEY (game_platform_id) REFERENCES platform(id);
+
+CREATE TABLE IF NOT EXISTS producer (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    producer_name VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS game_producer (
+    game_id INT NOT NULL,
+    producer_id INT NOT NULL
+);
+
+ALTER TABLE game_producer ADD FOREIGN KEY (game_id) REFERENCES game(id);
+ALTER TABLE game_producer ADD FOREIGN KEY (producer_id) REFERENCES producer(id);
+
+CREATE TABLE IF NOT EXISTS publisher (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    publisher_name VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS game_publisher (
+    game_id INT NOT NULL,
+    publisher_id INT NOT NULL
+);
+
+ALTER TABLE game_publisher ADD FOREIGN KEY (game_id) REFERENCES game(id);
+ALTER TABLE game_publisher ADD FOREIGN KEY (publisher_id) REFERENCES publisher(id);
