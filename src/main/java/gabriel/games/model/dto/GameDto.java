cee6fee71a12e13d177380a322bfff62cfd44523
Gabriel <gab.mos92@gmail.com>
2021-11-17 14:49:39 +0100

    added i18n to GameDto

--- src/main/java/gabriel/games/model/dto/GameDto.java
+++ src/main/java/gabriel/games/model/dto/GameDto.java
@@ -15,41 +15,41 @@ import java.util.List;
 public class GameDto {
 
     @NotNull
-    @Size(min = 1, max = 128)
+    @Size(min = 1, max = 128, message = "{uri.size}")
     private final String uri;
 
     @NotNull
-    @Size(min = 1, max = 128)
+    @Size(min = 1, max = 128, message = "{name.size}")
     private final String name;
 
     @NotNull
-    @Size(max = 1024)
+    @Size(max = 1024, message = "{description.size}")
     private final String description;
 
     @NotNull
-    @Size(max = 256)
+    @Size(max = 256, message = "{webpage.size}")
     private final String webpage;
 
     @NotNull
-    @Range(min = 0L, max = 10L)
+    @Range(min = 0L, max = 10L, message = "{playerRating.size}")
     private final Double playerRating;
 
     @NotNull
-    @Range(min = 0L, max = 10L)
+    @Range(min = 0L, max = 10L, message = "{reviewerRating.size}")
     private final Double reviewerRating;
 
     @NotNull
-    @Size(min = 1)
+    @Size(min = 1, message = "{platform.size}")
     private final List<@NotNull @Size(min = 1, max = 50) String> platforms;
 
     @NotNull
     private final Date releaseDate;
 
     @NotNull
-    @Size(max = 128)
+    @Size(max = 128, message = "{producer.size}")
     private final String producer;
 
     @NotNull
-    @Size(max = 128)
+    @Size(max = 128, message = "{publisher.size}")
     private final String publisher;
 }
