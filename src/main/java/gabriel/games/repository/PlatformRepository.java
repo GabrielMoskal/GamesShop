4449439b849a38285178a649c58c7cb4c373ee56
Gabriel <gab.mos92@gmail.com>
2021-12-30 15:31:16 +0100

    fixed PlatformRepository

--- src/main/java/gabriel/games/repository/PlatformRepository.java
+++ src/main/java/gabriel/games/repository/PlatformRepository.java
@@ -3,10 +3,12 @@ package gabriel.games.repository;
 import gabriel.games.model.api.Platform;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface PlatformRepository extends CrudRepository<Platform, Long> {
     Optional<Platform> findByUri(String uri);
 
+    @Transactional
     void deleteByUri(String uri);
 }
